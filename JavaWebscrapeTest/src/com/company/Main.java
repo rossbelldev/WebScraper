564973e5b6bd0eeb569a9b475bc3cdf645d6aefa
theRGTgamer <40317736@live.napier.ac.uk>
2020-01-20 19:08:05 +0000

    first commit

---
+++ JavaWebscrapeTest/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String url = "https://www.hltv.org/matches";
+        Document document = Jsoup.connect(url).userAgent("JeanRean").get();
+
+        Elements matches = document.select(".matches .upcoming-matches .upcoming-match");
+        for (Element match : matches) {
+            //Need to have a way to break up the teams, time, event, remove the A. then print between the --
+            Element name1 = document.select(".matches .upcoming-matches .upcoming-match .team").first();
+            System.out.println(name1);
+
+            System.out.println("------------------------------------------------------");
+            System.out.println(match.text());
+            System.out.println("------------------------------------------------------\n");
+        }
+
+        /* Shows each match for the number of days which there are (not the ideal thing)
+        Elements days = document.select(".matches .match-day");
+        for(Element day : days){
+            Elements matches = document.select(".matches .upcoming-matches .upcoming-match");
+            for (Element match : days) {
+                System.out.println(match.text());
+            }
+        }
+        */
+
+    }
+
+}
