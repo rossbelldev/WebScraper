bd88bf203f653041f2601202895d34f8a0af92a5
theRGTgamer <40317736@live.napier.ac.uk>
2020-01-20 23:00:39 +0000

    making progress

--- JavaWebscrapeTest/src/com/company/Main.java
+++ JavaWebscrapeTest/src/com/company/Main.java
@@ -11,27 +11,53 @@ public class Main {
         String url = "https://www.hltv.org/matches";
         Document document = Jsoup.connect(url).userAgent("JeanRean").get();
 
+        Elements links = document.select(".matches .match a");
+        for (Element link : links) {
+
+            String relHref = link.absUrl("href");
+            Document doc = Jsoup.connect(relHref).userAgent("JeanRean").get();
+
+            Elements tNames = doc.select(".match-page .teamName");
+            for(Element tName : tNames){
+                System.out.println(tName.text());
+            }
+
+
+
+        /*
         Elements matches = document.select(".matches .upcoming-matches .upcoming-match");
         for (Element match : matches) {
             //Need to have a way to break up the teams, time, event, remove the A. then print between the --
             Element name = document.select(".matches .upcoming-matches .upcoming-match .team").first();
             String nameOnly = name.text();
 
+            Elements links = document.select(".matches .match a");
+            for(Element link : links){
+                //Need to reorganise completely. Needs to have
+                String relHref = link.absUrl("href");
+                Document doc = Jsoup.connect(relHref).userAgent("JeanRean").get();
+                Elements tNames = doc.select(".match-page .teamName");
+
+                for(Element tName : tNames){
+                    System.out.println(tName.text());
+                    System.out.println("\n");
+                }
+            }
+            */
+
+
+
+
+
+
+            /*
             System.out.println("------------------------------------------------------");
             System.out.println(nameOnly);
             System.out.println(match.text());
+            System.out.println(relHref);
             System.out.println("------------------------------------------------------\n");
+             */
         }
-
-        /* Shows each match for the number of days which there are (not the ideal thing)
-        Elements days = document.select(".matches .match-day");
-        for(Element day : days){
-            Elements matches = document.select(".matches .upcoming-matches .upcoming-match");
-            for (Element match : days) {
-                System.out.println(match.text());
-            }
-        }
-        */
 
     }
 
